53767436b4ba7e8f6cf6c7238f05ab83c5e95543
RevathyBD <revathy@vvce.ac.in>
2025-02-25 15:16:02 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,12 @@
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+
+        // Adding two numbers
+        int sum = num1 + num2;
+
+        // Displaying the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
